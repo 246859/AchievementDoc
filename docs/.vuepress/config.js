6ee2486d76bf3ecc5c201a76a016451f7fa0af7f
246859 <2633565580@qq.com>
2022-10-08 15:09:40 +0800

    docs(docs): 优化styl，修改配置
    BREAKING CHANGE: none
    
    Closes none

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,31 +2,32 @@ const {generateSideGroup} = require('./utils/index')
 const root = './docs';
 
 module.exports = {
-    theme: "vt",
+    theme: 'vt',
     base: '/AchievementDocs/',
     markdown: {
         lineNumbers: true
     },
-    plugins: [
-        ['@vuepress/back-to-top'],
-    ],
     title: 'AchievementDoc',
     description: 'BDS llse plugins Achievement',
     head: [
+        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
         [
             'link',
             {rel: 'icon', href: 'Achievement.svg'}
         ]
     ],
+    plugins: [
+        ['@vuepress/back-to-top'],
+    ],
     themeConfig: {
-        enableDarkMode: true,
+        enableDarkMode:true,
         lastUpdated: '最后更新于', // string | boolean
         nav: [
             {text: 'Home', link: '/'},
-            {text: '指南', link: '/guide'},
+            {text: '指南🧭', link: '/guide'},
             {text: 'Q&A', link: '/question'},
             {text: 'Github', link: 'https://github.com/246859/Achievement.git'},
-            {text: '编辑', link: 'https://github.com/246859/AchievementDoc'},
+            {text: '编辑✍', link: 'https://github.com/246859/AchievementDoc'},
             {
                 text: '站点', items: [
                     {text: '国外', link: 'https://246859.github.io/AchievementDocs/'},
